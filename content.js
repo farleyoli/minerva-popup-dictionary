10c1b808a318cd02e7c28418de1b3b3f67d399e5
farleyoli <farleyoli@hotmail.com>
2021-01-20 19:18:02 -0300

    Transform definition to HTML

--- content.js
+++ content.js
@@ -1,38 +1,50 @@
 // content.js
-// TODO: create pop-up; transform dictionary definitions to html; when doing it, get id of each acception (path in tree) and add it to id of div; use css-flex; create connection to anki-connect; get phrase, set parameters and construct
+// TODO: transform dictionary definitions to html; when doing it, get id of each acception (path in tree) and add it to id of div; use css-flex; create connection to anki-connect; get phrase, set parameters and construct
 
+function isFirstLetterCapital(word) {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    if (word.length == 0) {
+        return false;
+    }
+    if (letters.indexOf(word[0]) >= 0) {
+        return true;
+    }
+    return false;
+}
 
-function constructPopup(x, y, width, height, text) {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode(text);
-    newDiv.appendChild(newContent);
-    newDiv.style.position = 'fixed';
-    newDiv.style.top = x.toString() + 'px';
-    newDiv.style.left = y.toString() + 'px';
-    newDiv.style.width = width.toString() + 'px';
-    newDiv.style.height = height.toString() + 'px';
-    newDiv.style.backgroundColor = 'white';
-    newDiv.style.zIndex = '1000';
-    newDiv.style.overflow = 'scroll';
-    newDiv.style.overflowX = 'hidden';
-    newDiv.style.overflowY = 'auto';
-    document.body.appendChild(newDiv);
+function constructPopup(x, y, width, height, dfnDiv) {
+    //const newDiv = document.createElement("div");
+    dfnDiv.style.position = 'fixed';
+    dfnDiv.style.fontSize = '10px';
+    dfnDiv.style.top = x.toString() + 'px';
+    dfnDiv.style.left = y.toString() + 'px';
+    dfnDiv.style.width = width.toString() + 'px';
+    dfnDiv.style.height = height.toString() + 'px';
+    dfnDiv.style.backgroundColor = 'white';
+    dfnDiv.style.zIndex = '1000';
+    dfnDiv.style.overflow = 'scroll';
+    dfnDiv.style.overflowX = 'hidden';
+    dfnDiv.style.overflowY = 'auto';
+    dfnDiv.style.borderRadius = '10px';
+    document.body.appendChild(dfnDiv);
 }
 
 function constructDfn(dict, word) {
+    const retDiv = document.createElement("div");
     // Use the word "acid" to test stuff.
-    function constructDfnAux(upperDfn, dfn) {
+    function constructDfnAux(dfn) {
+        const ret = document.createElement("div");
         if (dfn.length <= 0) {
-            return upperDfn;
+            return ret;
         }
-        let s = " ";
         for (let i = 0; i < dfn.length; i++) {
-            let loopRet = upperDfn + " ";
-            ctnt = dfn[i]['ctnt'];
-            loopRet += constructDfnAux(ctnt, dfn[i]['dfn']);
-            s += loopRet + "\n";
+            const textNode = document.createElement("div");
+            textNode.innerText = dfn[i]['ctnt'];
+            ret.appendChild(textNode);
+            ret.appendChild(constructDfnAux(dfn[i]['dfn']));
         }
-        return s;
+        ret.style.marginLeft = "5%";
+        return ret;
     }
     /*
     dict[word] is an array.
@@ -45,13 +57,15 @@ function constructDfn(dict, word) {
         see definition of head. (ex: head(tested) = test)
     */
     if (!(word in dict)) {
-        return "Word not found in dictionary.";
+        retDiv.innerText = "Word not found in dictionary.";
+        return retDiv;
     }
-    let ret = "";
+    //return retDiv;
+
     for (let i = 0; i < dict[word].length; i++) {
-        ret += constructDfnAux("", dict[word][i]['dfn']) + "\n";
+        retDiv.appendChild(constructDfnAux(dict[word][i]['dfn']));
     }
-    return ret;
+    return retDiv;
 }
 
 function getFirstLetter(word) {
@@ -62,18 +76,21 @@ function getFirstLetter(word) {
     if (letters.indexOf(word[0].toLowerCase()) >= 0) {
         return word[0].toLowerCase();
     }
+
     return "other";
 }
 
+
 function getDefinition(word) {
     let firstLetter = getFirstLetter(word);
+    console.log(firstLetter);
     let dictAdress = './dictionary/' + firstLetter + '.json';
     const url = chrome.runtime.getURL(dictAdress);
     fetch(url)
         .then((response) => response.json())
         .then((dict) => { 
-            let dfn = constructDfn(dict, word);
-            constructPopup(100, 100, 300, 100, dfn);
+            let dfnDiv = constructDfn(dict, word);
+            constructPopup(100, 100, 300, 100, dfnDiv);
             //alert(dfn);
         });
 }
