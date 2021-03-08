dc980524125eb5e66f96174014d94c0be06dd52c
farleyoli <farleyoli@hotmail.com>
2021-03-08 14:03:13 -0300

    Fix some things related to Ankiconnect.

--- anki.js
+++ anki.js
@@ -1,1 +1,37 @@
-//See https://github.com/ninja33/ODH/blob/master/src/bg/js/ankiconnect.js for reference
+// This has to be a background script.
+function invoke(action, version, params={}) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('error', () => reject('failed to issue request'));
+        xhr.addEventListener('load', () => {
+            try {
+                const response = JSON.parse(xhr.responseText);
+                if (Object.getOwnPropertyNames(response).length != 2) {
+                    throw 'response has an unexpected number of fields';
+                }
+                if (!response.hasOwnProperty('error')) {
+                    throw 'response is missing required error field';
+                }
+                if (!response.hasOwnProperty('result')) {
+                    throw 'response is missing required result field';
+                }
+                if (response.error) {
+                    throw response.error;
+                }
+                resolve(response.result);
+            } catch (e) {
+                reject(e);
+            }
+        });
+
+        xhr.withCredentials = true;
+        xhr.open('POST', 'http://127.0.0.1:8765');
+        xhr.send(JSON.stringify({action, version, params}));
+    });
+}
+console.log("testtest");
+async function test() {
+    const result = await invoke('deckNames', 6);
+    console.log(`got list of decks: ${result}`);
+}
+test();
